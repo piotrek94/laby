8d90fe57179a00a71eaab861622ab9af04519542
Your Name <you@example.com>
2018-11-20 18:20:25 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600"
